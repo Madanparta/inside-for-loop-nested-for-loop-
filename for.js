d0a2bf5e313fe478def0c3ed356c34b8cd340d12
madan k <partagowda15@gmil.com>
2022-06-28 23:50:31 +0530

    inside for loop

---
+++ for.js
@@ -0,0 +1,11 @@
+// 1.
+
+let num = 4;
+
+for(let i=1; i<=num; i++){
+  let star = ''
+  for(let j=1; j<=num+1; j++){
+    star=star+"*"
+  }
+  console.log(star)
+}
---
+++ readme.md
@@ -0,0 +1,13 @@
+# for inside the for loop
+
+`1.`Print the pattern given below, using loops.
+
+---
+
+---
+
+---
+
+---
+
+---
