1d1ac2b874ea20a971ed520b00cc20c4d7699835
madan k <partagowda15@gmil.com>
2022-07-01 17:41:50 +0530

    adding additionals.

--- for.js
+++ for.js
@@ -1,11 +1,75 @@
 // 1.
 
-let num = 4;
+// let num = 4;
 
-for(let i=1; i<=num; i++){
-  let star = ''
-  for(let j=1; j<=num+1; j++){
-    star=star+"*"
-  }
-  console.log(star)
-}
+// for(let i=1; i<=num; i++){
+//   let star = ''
+//   for(let j=1; j<=num+1; j++){
+//     star=star+" $"
+//   }
+//   console.log(star)
+// }
+
+
+// 2.
+// let n = 5;
+
+// for(let i=1; i<=n; i++){
+//   let star = ' '
+//   for(let j=1; j<=i; j++){
+//     star = star+ "  "
+//   }
+//   for(let k=i; k<=n; k++){
+//     star+=" $"
+//   }
+//   console.log(star)
+// }
+
+// 3.
+
+// let n= 5
+
+// for(let i=1; i<=n; i++){
+//   let star = ' '
+//   for(let j=i; j<=n; j++){
+//     star+="  "
+//   }
+//   for(let k=1; k<=i; k++){
+//     star+=" $"
+//   }
+//   for(let l=1; l<i; l++){
+//     star+=" $"
+//   }
+//   console.log(star)
+// }
+
+// 4.
+
+// let n= 5
+
+// for(let i=1; i<n; i++){
+//   let star = ' '
+//   for(let j=i; j<=n; j++){
+//     star+="  "
+//   }
+//   for(let k=1; k<=i; k++){
+//     star+=" $"
+//   }
+//   for(let l=1; l<i; l++){
+//     star+=" $"
+//   }
+//   console.log(star)
+// }
+// for(let i=1; i<=n; i++){
+//   let star = ' '
+//   for(let j=1; j<=i; j++){
+//     star+="  "
+//   }
+//   for(let k=i; k<=n; k++){
+//     star+=" $"
+//   }
+//   for(let l=i; l<n; l++){
+//     star+=" $"
+//   }
+//   console.log(star)
+// }
